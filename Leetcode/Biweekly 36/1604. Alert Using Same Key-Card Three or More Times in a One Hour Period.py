e47b091ac87eb263d0d1fa3eb5ea11d141e6338d
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2020-10-10 00:10:59 +0530

    Create 1604. Alert Using Same Key-Card Three or More Times in a One Hour Period.py

---
+++ Leetcode/Biweekly 36/1604. Alert Using Same Key-Card Three or More Times in a One Hour Period.py
@@ -0,0 +1,48 @@
+# Question Link:- https://leetcode.com/problems/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/
+
+class Solution:
+    def alertNames(self, name: List[str], time: List[str]) -> List[str]:
+        # number of cases in array.
+        n = len(time)
+        
+        # array which contains name of workers who received an alert.
+        arr = []
+        
+        # map which contains name as key and list of times(in minutes) as value.
+        mapper = {}
+        
+        for i in range(n):
+            hr,mm = list(map(int,time[i].split(':')))
+            # finding time in minutes
+            time_in_min = hr*60+mm
+            worker = name[i]
+            if worker in mapper:
+                mapper[worker].append(time_in_min)
+            else:
+                mapper[cur] = [time_in_min]
+        
+        # sorting the list(sorting time for each worker) of all keys in mapper
+        for i in mapper:
+            value = mapper[i]
+            value.sort()
+            mapper[i] = value
+        
+        
+        for i in mapper:
+            temp = d[i]
+            # possiblity of getting use of card 3 or more than 3 times
+            if len(temp)>=3:
+                st = 0
+                en = 2
+                while en<len(temp):
+                    if temp[en]-temp[st]<=60:
+                        arr.append(i)
+                        break
+                    en+=1
+                    st+=1
+        
+        if arr==[]:
+            return []
+        # sorting names in alphabetical order.
+        arr.sort()
+        return arr
