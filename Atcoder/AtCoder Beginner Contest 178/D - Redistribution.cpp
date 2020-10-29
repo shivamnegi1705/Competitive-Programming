1857a0ccca1e10529bb4f59a3180f6960531169b
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2020-10-29 22:22:12 +0530

    Create D - Redistribution.cpp

---
+++ Atcoder/AtCoder Beginner Contest 178/D - Redistribution.cpp
@@ -0,0 +1,115 @@
+// Question Link:- https://atcoder.jp/contests/abc178/tasks/abc178_d
+
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize ("-O3")
+#define ll long long  int
+#define all(x) (x).begin(), (x).end()
+using ld = long double;
+const ll mod = 1000000007;
+const ll inf = 1000000000000000000;
+const ll rk = 256;
+const ld PI = 3.141592653589793;
+#define pb push_back
+#define mp make_pair
+#define vc vector
+#define fs first
+#define sec second
+// #define pq priority_queue
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define pls pair<ll,string>
+#define psl pair<string,ll>
+#define plc pair<ll,char>
+#define pcl pair<char,ll>
+#define pss pair<string,string>
+#define all(x) (x).begin(), (x).end()
+#define tol(s) transform(s.begin(),s.end(),s.begin(),::tolower);
+#define tou(s) transform(s.begin(),s.end(),s.begin(),::toupper);
+#define print(a) for(auto j:a) {cout<<j<<",";} cout << "\n";
+#define endl '\n'
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+// getting string line
+// getline(cin,word);
+// -> priority_queue < int > pq; // max_heap
+// -> priority_queue < int , vector < int > , greater < int > > pq; // min_heap
+
+
+ostream & operator << (ostream & os, pair <ll, ll> const& x) {
+    os << x.first << "," << x.second;
+    return os;
+}
+
+template <class T>
+ostream & operator << (ostream & os, vector <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class T>
+ostream & operator << (ostream & os, set <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p) {
+    return os << "[ " << p.first << ", " << p.second << "]" ;
+}
+
+struct Triplet{
+    ll x,y,z;
+};
+
+ll modularexpo(ll x,ll n,ll mod){
+    ll res = 1;
+    while(n>0){
+        if(n%2==1){
+            res = (res*x)%mod;
+        }
+        x = (x*x);
+        n = n/2;
+    }
+    return res;
+}
+
+ll s;
+ll dp[2005];
+
+ll func(ll rem){
+    if(rem==0){
+        return 1;
+    }
+    if(dp[rem]!=-1){
+        return dp[rem];
+    }
+
+    ll ans = 0;
+    for(int i=3;i<2005;i++){
+        if(rem-i>=0){
+            ans = (ans+func(rem-i))%mod;
+        }
+        else{
+            break;
+        }
+    }
+    return dp[rem] = ans;
+}
+
+int main(){
+    cin>>s;
+    memset(dp,-1,sizeof(dp));
+    cout<<func(s)<<endl;
+    
+    return 0;
+}
