2641ccb45125c5ba27d1f4b6c9d6704c74d0bd33
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2020-11-16 22:42:59 +0530

    Create 1025. Divisor Game.cpp

---
+++ Dynamic Programming/Leetcode/1025. Divisor Game.cpp
@@ -0,0 +1,17 @@
+// Question Link:- https://leetcode.com/problems/divisor-game/description/
+
+class Solution {
+
+public:
+
+    bool divisorGame(int N) {
+
+        if(N&1)
+
+            return false;
+
+        return true;
+
+    }
+
+};
