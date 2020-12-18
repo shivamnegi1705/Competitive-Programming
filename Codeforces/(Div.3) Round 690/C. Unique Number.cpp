6313c7b9a1f53c0b9f795fddd03364952f02c105
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2020-12-18 23:29:03 +0530

    Create C. Unique Number.cpp

---
+++ Codeforces/(Div.3) Round 690/C. Unique Number.cpp
@@ -0,0 +1,126 @@
+// Question Link:- https://codeforces.com/contest/1462/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#pragma GCC optimize ("-O3")
+#define ll long long  int
+#define all(x) (x).begin(), (x).end()
+using ld = long double;
+const ll mod = 1000000007;
+const ll inf = 1e9;
+const ll rk = 256;
+const ld PI = 3.141592653589793;
+#define pb push_back
+#define mp make_pair
+#define vc vector
+#define fs first
+#define sec second
+// #define pq priority_queue
+#define lb lower_bound
+#define ub upper_bound
+#define pll pair<ll,ll>
+#define pls pair<ll,string>
+#define psl pair<string,ll>
+#define plc pair<ll,char>
+#define pcl pair<char,ll>
+#define pss pair<string,string>
+#define all(x) (x).begin(), (x).end()
+#define tol(s) transform(s.begin(),s.end(),s.begin(),::tolower);
+#define tou(s) transform(s.begin(),s.end(),s.begin(),::toupper);
+#define print(a) for(auto j:a) {cout<<j<<",";} cout << "\n";
+#define endl '\n'
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+// getting string line
+// getline(cin,word);
+// -> priority_queue < int > pq; // max_heap
+// -> priority_queue < int , vector < int > , greater < int > > pq; // min_heap
+
+
+ostream & operator << (ostream & os, pair <ll, ll> const& x) {
+    os << x.first << "," << x.second;
+    return os;
+}
+
+template <class T>
+ostream & operator << (ostream & os, vector <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class T>
+ostream & operator << (ostream & os, set <T> const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class Ch, class Tr, class Container>
+basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) {
+    os << "{ ";
+    for(auto& y : x) os << y << " ";
+    return os << "}";
+}
+
+template <class X, class Y>
+ostream & operator << (ostream & os, pair <X, Y> const& p) {
+    return os << "[ " << p.first << ", " << p.second << "]" ;
+}
+
+struct Triplet{
+    ll x,y,z;
+};
+
+ll modularexpo(ll x,ll n,ll mod){
+    ll res = 1;
+    while(n>0){
+        if(n%2==1){
+            res = (res*x)%mod;
+        }
+        x = (x*x);
+        n = n/2;
+    }
+    return res;
+}
+
+ll t,x;
+ll ans;
+void func(int d,int s,ll k){
+    if(d!=10){
+        if(s>=d){
+            func(d+1,s-d,k*10+d);
+        }
+        func(d+1,s,k);
+    }
+    else{
+        if(s==0){
+            ans = min(ans,k);
+        }
+    }
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+    FastIO;
+    cin>>t;
+    while(t--){
+        cin>>x;
+        ans = LLONG_MAX;
+        func(1,x,0);
+        if(ans!=LLONG_MAX){
+            string s = to_string(ans);
+            sort(all(s));
+            cout<<s<<endl;
+        }
+        else{
+            cout<<-1<<endl;
+        }
+    }
+    return 0;
+}
