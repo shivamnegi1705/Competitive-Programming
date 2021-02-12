bca895f52ddcba9f8d19eda6ae5fd2d8ab749c06
Shivam Negi <72339012+shivamnegi1705@users.noreply.github.com>
2021-02-12 20:13:08 +0530

    Create 1673. Find the Most Competitive Subsequences.py

---
+++ Leetcode/January Challenge 2021/1673. Find the Most Competitive Subsequences.py
@@ -0,0 +1,12 @@
+#Question Link- https://leetcode.com/problems/find-the-most-competitive-subsequence/description/
+class Solution:
+    def mostCompetitive(self, nums, k):
+        attempts = len(nums) - k
+        stack = []
+        for num in nums:
+            while stack and num < stack[-1] and attempts > 0:
+                stack.pop()
+                attempts -= 1
+            stack.append(num)
+        
+        return stack[:k]
